2f4d6a1bcd1973979af074661d62303c4017dd6a
Nathan Lim <nathanlim2004@gmail.com>
2024-08-25 15:14:52 -0700

    Add check-in success page

---
+++ src/app/checkinsuccess/page.jsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import WhiteTextButton from "../components/whitebutton"
+import "../style.css"
+
+export default function CheckInSuccess () {
+
+    const router = useRouter()
+
+    setTimeout( function () {
+        router.push("/")
+    }, 5000)
+
+    return (
+        <div className="centre-align">
+            <h1 className="page-title">
+                You are all checked in!
+            </h1>
+            <p>
+                Please find a seat or see staff for further directions.
+            </p>
+            <p>
+                This screen will automatically advance in 5 seconds.
+            </p>
+            <WhiteTextButton href="/existing" text="Check another player in"/>
+        </div>
+    )
+}
+
+async function AutoAdvanceScript () {
+
+    setTimeout(function() {
+        window.location.href = '..';
+    }, 5000);
+}
